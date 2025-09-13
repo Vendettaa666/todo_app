9709147b3b9f4827edd051a0e6fe7720eaa1f404
vendettaa666 <vinnfamily666@gmail.com>
2025-09-14 00:10:43 +0700

    fix: memunculkan halaman edit,show,create task list

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,7 +1,9 @@
-<x-app-layout>
+@extends('layouts.app')
+
+@section('content')
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <livewire:create-task />
     </div>
-</x-app-layout>
+@endsection
 
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
-<x-app-layout>
+@extends('layouts.app')
+
+@section('content')
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <livewire:task-detail :task="$task" />
     </div>
-</x-app-layout>
-
-
+@endsection
