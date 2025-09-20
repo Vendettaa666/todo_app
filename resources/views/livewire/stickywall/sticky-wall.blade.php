5a94b8113d921149a2e7244c5605f85f22dc2fd6
vendettaa666 <vinnfamily666@gmail.com>
2025-09-21 00:43:20 +0700

    fix: tampilan sticky wall

--- resources/views/livewire/stickywall/sticky-wall.blade.php
+++ resources/views/livewire/stickywall/sticky-wall.blade.php
@@ -1,23 +1,24 @@
 <div class="p-6">
-    <h1 class="text-3xl font-bold mb-6">Papan Catatan</h1>
-
-    <form wire:submit="saveNote" class="mb-8">
-        <textarea wire:model="newNoteContent" placeholder="Tulis catatan baru..."
-            class="w-full p-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
-        @error('newNoteContent')
-            <span class="text-red-500 text-sm">{{ $message }}</span>
-        @enderror
-        <button type="submit"
-            class="mt-2 px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition duration-300">
-            Simpan Catatan
-        </button>
-    </form>
-
+    <h1 class="text-3xl font-bold mb-2 text-white">Papan Catatan</h1>
+    <p class="text-gray-600">Tambahkan Papan Ringkaasan Anda</p>
+    <div class="mt-4">
+        <form wire:submit="saveNote" class="mb-8">
+            <textarea wire:model="newNoteContent" placeholder="Tulis catatan baru..."
+                class="w-full p-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+            @error('newNoteContent')
+                <span class="text-red-500 text-sm">{{ $message }}</span>
+            @enderror
+            <button type="submit"
+                class="mt-2 px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition duration-300">
+                Simpan Catatan
+            </button>
+        </form>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         @foreach ($notes as $note)
-            <div class="p-6 rounded-lg shadow-md relative" style="background-color: #fff9c4;">
+            <div class="p-6 rounded-sm shadow-md relative" style="background-color: #fff9c4;">
                 <p>{{ $note->content }}</p>
-                </div>
+            </div>
         @endforeach
     </div>
 </div>
